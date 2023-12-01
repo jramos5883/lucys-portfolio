077568f90274368d62204a5ceaa9809c29c66df3
jramos5883 <jramos5883@gmail.com>
2023-11-30 19:28:02 -0800

    Setting up to build out Lucy's portfolio

---
+++ lucys-website/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        // Custom Break Points
+        cellsm: "360px",
+        celllg: "412px",
+        tablet: "768px",
+        lapsm: "1024px",
+        laplg: "1366px",
+        lapxl: "1536px",
+        desksm: "1920px",
+        desklg: "2560px",
+      },
+    },
+  },
+  plugins: [],
+};
